6a7359b09459b26f20e8d29ffd1a437576d70fa6
Babita00 <babitaasth07@gmail.com>
2025-06-19 00:20:22 +0545

    feat: add CORS support and update dependencies in package.json and server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,21 @@ import { connectDB } from './config/db';
 import userRoutes from './routes/user.router';
 import eventRoutes from './routes/event.router';
 import eventFormROutes from './routes/eventForm.router';
+import announcementRoutes from './routes/announcement.router';
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/user', userRoutes);
 app.use('/api/event', eventRoutes);
 app.use('/api/form-events', eventFormROutes);
+app.use('/api/announcement', announcementRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
